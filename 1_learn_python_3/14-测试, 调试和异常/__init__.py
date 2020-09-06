e13bfd2f5255c1a9f2267cfcf9f3896816fee043
lixun <againstnow@gmail.com>
2020-09-06 11:45:45 +0800

    目录规划更改

---
+++ 1_learn_python_3/14-测试, 调试和异常/__init__.py
@@ -0,0 +1,16 @@
+#!/data1/Python-2.7.4/bin/python2.7
+# -*-coding:utf-8-*-
+# time: 2020-09-06 11:45 
+# user: lixun
+# filename: __init__.py
+# description: 
+# 
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    
